c08dbd957ded15657b9bf59cd28dbe9e6b5a3bc9
albert-kevin <beire_@hotmail.com>
2022-11-13 22:56:28 +0100

    add weblink and token variable for openai

--- mytoken-sample.py
+++ mytoken-sample.py
@@ -1,1 +1,9 @@
+# https://replicate.com/account
+# register/login and generate your token
+# also make sure you have enough 'credits'
 REPLICATE_API_TOKEN = ""
+
+# https://openai.com/api/
+# register/login and generate your token
+# also make sure you have enough 'credits'
+OPENAI_API_TOKEN = ""
